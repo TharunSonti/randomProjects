5cbba4ea6146ca5673d90176ed32c381cecd78e6
TharunSonti <tharun.sonti@uq.net.au>
2016-09-25 00:09:09 +1000

    Webpage link error fixed! Bloody 'Meta_refresh'...
    Had a problem with accessing the library catalogue: 
    
    I thought the webpage for the library catalogue was:   https://elibcat.library.brisbane.qld.gov.au
    
    So I used the homepage but no forms found on this page.. I wondered why. I printed the HTML for the code which was very small.. Which didn't make sense.. 
    
    In the browser this link actually took us elsewhere.. e.g. to https://elibcat.library.brisbane.qld.gov.au/uhtbin/cgisirsi/?ps=ohIqbErG80/ZZELIBCAT/1640104/60/1308/X
    
    In using this link in mechanize it wouldn't redirect (not if I just ran br.open(url)  anyway.. 
    
    So I thought using that long redirect link (that I got from the browser) would fix the problem in our code. 
    
    Worked once or twice? Didn't work after that.. Perhaps because I also searched in my browser on this link and 'used it up'.. So then this code wouldn't run properly - webpage had changed? 
    
    Turns out it's a bit more complicated.. I checked out the HTML for the homepage: https://elibcat.library.brisbane.qld.gov.au
    
    Noticed it was so small! 
    
    <HTML>
    <HEAD>
    <META HTTP-EQUIV="Refresh" CONTENT="0; URL=/uhtbin/webcat">
    </HEAD>
    <BODY>
    </BODY>
    </HTML>
    
    Notice the "URL=/uhtbin/webcat"    I've seen links that start with a '/' before.. You just tack this on to the end of the home page.. 
    
    So I tried adding this on to the end of the home page url - thus giving us: https://elibcat.library.brisbane.qld.gov.au/uhtbin/webcat
    
    This worked in code + in the browser! 
    
    On investigating the meta http-equiv = "refresh" I found this useful link
    https://en.wikipedia.org/wiki/Meta_refresh
    
    The below explanation on the wiki page tells us what we need to know: 
    "...Used as a redirect..  It is also possible to instruct the browser to fetch a different URL when the page is refreshed, by including the alternative URL in the content parameter. By setting the refresh time interval to zero (or a very low value), this allows meta refresh to be used as a method of URL redirection.
    
    This is exactly the pattern we see in the html for https://elibcat.library.brisbane.qld.gov.au/:
    <META HTTP-EQUIV="Refresh" CONTENT="0; URL=/uhtbin/webcat"> 
    
    
    Sweet. So I now understand this a lot better + things are working!

--- librarybooks.py
+++ librarybooks.py
@@ -2,21 +2,14 @@ from bs4 import BeautifulSoup
 import mechanize
 
 #started 9:35 pm 24/9/2016
-
-bookslist = []
-
-
-url = 'https://elibcat.library.brisbane.qld.gov.au/uhtbin/cgisirsi/?ps=ohIqbErG80/ZZELIBCAT/1640104/60/1308/X'
-#worked but then failed later? 
-
-#I'll have to go to main page then follow the link? 
-#url = 'https://elibcat.library.brisbane.qld.gov.au'
-
 #http://stackoverflow.com/questions/4888463/how-to-install-mechanize-for-python-2-7
 #sudo apt-get install python-mechanize
 
+bookslist = []
 
-print url
+url = 'https://elibcat.library.brisbane.qld.gov.au/uhtbin/webcat'
+
+#print url
 
 br = mechanize.Browser()
 br.set_handle_robots(False)   # ignore robots
@@ -25,20 +18,8 @@ br.set_handle_refresh(False)  # can sometimes hang without this
 response = br.open(url)
 #print response.read() 
 
-for link in br.links():
-    print link.text, link.url
-    
 for form in br.forms():
     print "Form name:", form.name
-    #print form
-
-#Form name: new_session
-#Form name: loginform
-#Form name: searchform
-#Form name: reset_query_form
-#Form name: url_form
-
-#form we want is search form
 
 br.select_form("searchform")
 
@@ -59,34 +40,23 @@ for control in br.form.controls:
     print control 
     
 response = br.submit()
-print response.read()
 
-#<TextControl(searchdata1=)>
-#<SelectControl(srchfield1=[*GENERAL^SUBJECT^GENERAL^^words or phrase, AU^AUTHOR^AUTHORS^Author Processing^author, TI^TITLE^TITLES^Title Processing^title, SU^SUBJECT^SUBJECTS^^subject, SER^SERIES^TITLES^Title Processing^series, PER^PERTITLE^TITLES^Title Processing^periodical title])>
-#<SubmitControl(<None>=Search) (readonly)>
-#<SelectControl(library=[*ALL, SALL, ANNERLEY, ARC, ASHGROVE, BANYO, BRACKENRDG, BRISBANE S, BULIMBA, CARINA, CARINDALE, CHERMSIDE, COOPERS PL, CORINDA, EVERTON PA, FAIRFIELD, GARDEN CIT, GRANGE, HAMILTON, HOLLAND PA, INALA, INDOOROOPI, KENMORE, MITCHELTON, MOBILE, MT COOT-TH, MT GRAVATT, MT OMMANEY, NEW FARM, NUNDAH, SANDGATE, STONES COR, SUNNY BANK, TOOWONG, WEST END, WYNNUM, ZILLMERE])>
+#works and follows the url to where it goes  e.g.    https://elibcat.library.brisbane.qld.gov.au/uhtbin/webcat
+# redirected to 
+# https://elibcat.library.brisbane.qld.gov.au/uhtbin/cgisirsi/?ps=ohIqbErG80/ZZELIBCAT/1640104/60/1308/X
 
-#print br.form.controls[0].type()    #doesn't work
-
-####error when using single quotes e.g.  'book thief' instead of double quotes?
-#Traceback (most recent call last):
-#  File "librarybooks3.py", line 44, in <module>
-#    control.value = ['the book theif']
-#  File "/usr/lib/python2.7/dist-packages/mechanize/_form.py", line 1217, in __setattr__
-#    raise TypeError("must assign a string")
-#TypeError: must assign a string
-
-
-    ####error again    turns out double quotes wasn't the problem... expecting a string but we gave list.. ['book thief'] or ["book theif"] is a problem
-    # 'book thief'   without the square brackets works..!
-
-
-####output after correcting errors.. 
-#<TextControl(searchdata1=the book theif)>
+###########OUTPUT that was printed: ####################
+#Form name: new_session
+#Form name: loginform
+#Form name: searchform
+#Form name: reset_query_form
+#Form name: url_form
+#<TextControl(searchdata1=48 laws of power)>
 #<SelectControl(srchfield1=[GENERAL^SUBJECT^GENERAL^^words or phrase, AU^AUTHOR^AUTHORS^Author Processing^author, *TI^TITLE^TITLES^Title Processing^title, SU^SUBJECT^SUBJECTS^^subject, SER^SERIES^TITLES^Title Processing^series, PER^PERTITLE^TITLES^Title Processing^periodical title])>
 #<SubmitControl(<None>=Search) (readonly)>
-#<SelectControl(library=[ALL, SALL, ANNERLEY, ARC, ASHGROVE, BANYO, BRACKENRDG, BRISBANE S, BULIMBA, CARINA, *CARINDALE, CHERMSIDE, COOPERS PL, CORINDA, EVERTON PA, FAIRFIELD, GARDEN CIT, GRANGE, HAMILTON, HOLLAND PA, INALA, INDOOROOPI, KENMORE, MITCHELTON, MOBILE, MT COOT-TH, MT GRAVATT, MT OMMANEY, NEW FARM, NUNDAH, SANDGATE, STONES COR, SUNNY BANK, TOOWONG, WEST END, WYNNUM, ZILLMERE])>
+#<SelectControl(library=[*ALL, SALL, ANNERLEY, ARC, ASHGROVE, BANYO, BRACKENRDG, BRISBANE S, BULIMBA, CARINA, CARINDALE, CHERMSIDE, COOPERS PL, CORINDA, EVERTON PA, FAIRFIELD, GARDEN CIT, GRANGE, HAMILTON, HOLLAND PA, INALA, INDOOROOPI, KENMORE, MITCHELTON, MOBILE, MT COOT-TH, MT GRAVATT, MT OMMANEY, NEW FARM, NUNDAH, SANDGATE, STONES COR, SUNNY BANK, TOOWONG, WEST END, WYNNUM, ZILLMERE])>
 #<HiddenControl(sort_by=-PBYR) (readonly)>
 
+## So redirect worked, form was accessed and modified! 
 
-#* tells us which value is selected (that's what that cheatsheet said and it's true!)
+#print response.read()
